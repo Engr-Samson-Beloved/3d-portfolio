c13a42e1141359736f9f0b8fd5e2db27fccc8478
Eliab <eliabsiji@gmail.com>
2025-05-10 14:15:55 +0100

    made lots of change v4

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -54,10 +54,10 @@ const AppShowcase = () => {
             <div className="text-content">
               <h2>
                 On-Demand Rides Made Simple with a Powerful, User-Friendly App
-                called Ryde
+                called SkoolConnectNG
               </h2>
               <p className="text-white-50 md:text-xl">
-                An app built with React Native, Expo, & TailwindCSS for a fast,
+                An app built with NextJs Capacitor, Expo, & TailwindCSS for a fast,
                 user-friendly experience.
               </p>
             </div>
@@ -71,7 +71,7 @@ const AppShowcase = () => {
                   alt="Library Management Platform"
                 />
               </div>
-              <h2>The Library Management Platform</h2>
+              <h2>The Thrive Africa</h2>
             </div>
 
             <div className="project" ref={ycDirectoryRef}>
